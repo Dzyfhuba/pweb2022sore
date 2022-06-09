1517ab539cf5cf1994ec3c3b7e9c6cf5bb2501b1
Dzyfhuba <hafidz21ub@gmail.com>
2022-06-09 10:02:06 +0700

    prepare for crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CobaController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,5 @@ Route::get('/', function () {
 });
 
 Route::get('coba', [CobaController::class, 'index']);
+
+Route::get('/students', [StudentController::class, 'index']);
